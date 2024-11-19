074789e1a7e089dbb9296f478367e0864b8f0d8c
annguyen34 <21120508@student.hcmus.edu.vn>
2024-11-19 10:54:08 +0700

    feat: add jwt and paseto token maker

---
+++ token/jwt_maker.go
@@ -0,0 +1,65 @@
+package token
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+const minSecretKeySize = 32
+
+type JWTMaker struct {
+	secretKey string
+}
+
+func NewJWTMaker(secretKey string) (Maker, error) {
+	if len(secretKey) < minSecretKeySize {
+		return nil, fmt.Errorf("invalid secret key size, must be at least %d characters", minSecretKeySize)
+	}
+
+	return &JWTMaker{secretKey: secretKey}, nil
+}
+
+func (maker *JWTMaker) CreateToken(username string, duration time.Duration) (string, error) {
+	payload, err := NewPayload(username, duration)
+	if err != nil {
+		return "", err
+	}
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id":         payload.ID,
+		"username":   payload.Username,
+		"issued_at":  payload.IssuedAt.Unix(),
+		"expired_at": payload.ExpiredAt.Unix(),
+	})
+	return jwtToken.SignedString([]byte(maker.secretKey))
+}
+
+func (maker *JWTMaker) VerifyToken(token string) (*Payload, error) {
+	jwtToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, fmt.Errorf("invalid token signing method")
+		}
+		return []byte(maker.secretKey), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := jwtToken.Claims.(jwt.MapClaims)
+	if !ok || !jwtToken.Valid {
+		return nil, fmt.Errorf("invalid token")
+	}
+
+	return &Payload{
+		ID:        uuid.MustParse(claims["id"].(string)),
+		Username:  claims["username"].(string),
+		IssuedAt:  time.Unix(int64(claims["issued_at"].(float64)), 0),
+		ExpiredAt: time.Unix(int64(claims["expired_at"].(float64)), 0),
+	}, nil
+
+}
---
+++ token/paseto_maker_test.go
@@ -0,0 +1,34 @@
+package token
+
+import (
+	"testing"
+	"time"
+
+	"github.com/annguyen34/simple-bank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPasetoMaker(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	if err != nil {
+		t.Error(err)
+	}
+
+	username := util.RandomOwner()
+	duration := time.Minute
+	issuedAt := time.Now()
+	expriedAt := issuedAt.Add(duration)
+
+	token, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+
+	require.Equal(t, username, payload.Username)
+	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
+	require.WithinDuration(t, expriedAt, payload.ExpiredAt, time.Second)
+	require.NotZero(t, payload.ID)
+}
